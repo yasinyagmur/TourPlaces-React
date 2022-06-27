8471cde98b574d0defd1b11f969564c0f96add3f
yasin yağmur <yasin.yagmur@gmail.com>
2022-06-27 13:10:17 +0300

    github first react project

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import './Main.scss';
+import { data } from '../../helpers/data';
+import Card from './Card';
+const Main = () =>{
+    return(
+        <div className="card-container">
+            {data.map((item, index)=>  <Card {...item} key={index} /> )}
+        </div>
+    )
+}
+
+
+export default Main;
